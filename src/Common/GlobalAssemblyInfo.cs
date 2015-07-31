71b80de8d23a3be3dd014aa6cab28b10a372a91b
Patrick Sadowski <gummiholzbein@live.de>
2015-07-31 09:42:12 +0200

    Create solution and projects

---
+++ src/Common/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyProduct("NuGet.Dgml")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Patrick Sadowski 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: CLSCompliant(true)]
---
+++ src/NuGet.Dgml/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NuGet.Dgml")]
+[assembly: AssemblyDescription("")]
