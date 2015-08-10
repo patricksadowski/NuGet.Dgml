2cb5b3736cc1bd624137f02b4dddcc964f741168
Patrick Sadowski <gummiholzbein@live.de>
2015-08-10 17:53:01 +0200

    Add DirectedGraphExtensions

---
+++ test/NuGet.Dgml.Test/Dgml/DirectedGraphExtensionsFacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NuGet.Dgml
+{
+    public class DirectedGraphExtensionsFacts
+    {
+        public class AsXDocument
+        {
+            /* Only basic tests were implemented. We assume that XmlSerializer
+             * and the xsd tool are working properly.
+             */
+
+            [Fact]
+            public void ThrowsOnNull()
+            {
+                DirectedGraph graph = null;
+                Assert.Throws<ArgumentNullException>("graph", () => graph.AsXDocument());
+            }
+
+            [Fact]
+            public void GeneratesXDocument()
+            {
+                DirectedGraph graph = new DirectedGraph();
+                graph.Nodes = new[] { new DirectedGraphNode(), };
+                var document = graph.AsXDocument();
+                Assert.Equal(1, document.Root.Elements().Count());
+            }
+        }
+    }
+}
