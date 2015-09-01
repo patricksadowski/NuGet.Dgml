c52920d2fcc2648cf6cf86fd9640add2f0d115c7
Patrick Sadowski <gummiholzbein@live.de>
2015-09-02 00:04:22 +0200

    Add PackageUpgradeVisualizer

---
+++ src/NuGet.Dgml/Dgml/PackageUpgradeVisualizer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NuGet.Dgml
+{
+    /// <summary>
+    /// Visualizes packages upgrades in a directed graph.
+    /// </summary>
+    public class PackageUpgradeVisualizer
+    {
+        private readonly DirectedGraph directedGraph;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PackageUpgradeVisualizer"/> class.
+        /// </summary>
+        /// <param name="directedGraph">The directed graph used to visualize package upgrades.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="directedGraph"/> is <c>null</c>.</exception>
+        public PackageUpgradeVisualizer(DirectedGraph directedGraph)
+        {
+            if (directedGraph == null)
+            {
+                throw new ArgumentNullException(nameof(directedGraph));
+            }
+
+            this.directedGraph = directedGraph;
+        }
+
+        /// <summary>
+        /// Visualizes the specified package and its upgrades in the directed graph.
+        /// </summary>
+        /// <param name="package">The package to visualize.</param>
+        /// <param name="upgrades">The upgrades of the specified package.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="package"/> is <c>null</c>.</exception>
+        public void Visualize(IPackage package, IEnumerable<PackageUpgrade> upgrades)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            var packageNode = EnsurePackageNode(package);
+            ConfigurePackageNode(packageNode, package);
+
+            if (upgrades != null)
+            {
+                foreach (var upgrade in upgrades)
+                {
+                    var dependencyNode = EnsureDependencyNode(upgrade);
+                    var link = CreateLink(packageNode, dependencyNode);
+                    ConfigureLink(link, upgrade);
+                }
+            }
+        }
+
+        private DirectedGraphNode EnsurePackageNode(IPackage package)
+        {
+            var nodeId = GetNodeId(package);
+            return EnsureNode(nodeId);
+        }
+
+        private static void ConfigurePackageNode(DirectedGraphNode node, IPackage package)
+        {
+            if (!package.IsReleaseVersion())
+            {
+                node.Background = "Gainsboro";
+            }
+        }
+
+        private DirectedGraphNode EnsureDependencyNode(PackageUpgrade upgrade)
+        {
+            string nodeId = GetNodeId(upgrade);
+            return EnsureNode(nodeId);
+        }
+
+        private static string GetNodeId(PackageUpgrade upgrade) => upgrade.Package != null
+                ? GetNodeId(upgrade.Package)
+                : GetNodeId(upgrade.PackageDependency);
+
+        private static string GetNodeId(IPackage package) => package.GetFullName();
+
+        private static string GetNodeId(PackageDependency packageDependency) => packageDependency.Id;
+
+        private DirectedGraphNode EnsureNode(string id)
+        {
+            EnsureNodes();
+
+            var node = GetNode(id);
+            if (node == null)
+            {
+                node = CreateNode(id);
+                node.Label = id;
+                AddNode(node);
+            }
+
+            return node;
+        }
+
+        private void EnsureNodes()
+        {
+            if (directedGraph.Nodes == null)
+            {
+                directedGraph.Nodes = new DirectedGraphNode[0];
+            }
+        }
+
+        private DirectedGraphNode GetNode(string packageId) => directedGraph.Nodes.FirstOrDefault(n => packageId.Equals(n.Id));
+
+        private DirectedGraphNode CreateNode(string packageId)
+        {
+            var node = new DirectedGraphNode();
+            node.Id = packageId;
+            return node;
+        }
+
+        private void AddNode(DirectedGraphNode node)
+        {
+            var nodes = directedGraph.Nodes;
+            var nodeIndex = nodes.Length;
+            Array.Resize(ref nodes, nodeIndex + 1);
+            nodes[nodeIndex] = node;
+            directedGraph.Nodes = nodes;
+        }
+
+        private DirectedGraphLink CreateLink(DirectedGraphNode source, DirectedGraphNode target)
+        {
+            var link = new DirectedGraphLink();
+            link.Source = source.Id;
+            link.Target = target.Id;
+            AddLink(link);
+            return link;
+        }
+
+        private void AddLink(DirectedGraphLink link)
+        {
+            EnsureLinks();
+
+            var links = directedGraph.Links;
+            var linkIndex = links.Length;
+            Array.Resize(ref links, linkIndex + 1);
+            links[linkIndex] = link;
+            directedGraph.Links = links;
+        }
+
+        private void EnsureLinks()
+        {
+            if (directedGraph.Links == null)
+            {
+                directedGraph.Links = new DirectedGraphLink[0];
+            }
+        }
+
+        private void ConfigureLink(DirectedGraphLink link, PackageUpgrade upgrade)
+        {
+            link.Label = upgrade.PackageDependency.VersionSpec.ToString();
+            link.Stroke = GetStroke(upgrade.Action);
+        }
+
+        private string GetStroke(PackageUpgradeAction action)
+        {
+            switch (action)
+            {
+                case PackageUpgradeAction.None:
+                    return "Black";
+                case PackageUpgradeAction.MinVersion:
+                    return "ForestGreen";
+                case PackageUpgradeAction.ReleaseToRelease:
+                    return "Goldenrod";
+                case PackageUpgradeAction.PrereleaseToRelease:
+                    return "DarkOrange";
+                case PackageUpgradeAction.PrereleaseToPrerelease:
+                    return "OrangeRed";
+                case PackageUpgradeAction.ReleaseToPrerelease:
+                    return "Firebrick";
+            }
+            return null;
+        }
+    }
+}
---
+++ tests/NuGet.Dgml.Tests/Dgml/PackageUpgradeVisualizerFacts.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace NuGet.Dgml
+{
+    public class PackageUpgradeVisualizerFacts
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void ThrowsOnNull()
+            {
+                Assert.Throws<ArgumentNullException>("directedGraph", () => new PackageUpgradeVisualizer(null));
+            }
+        }
+
+        public class Visualize
+        {
+            private readonly DirectedGraph directedGraph;
+            private readonly PackageUpgradeVisualizer visualizer;
+
+            public Visualize()
+            {
+                directedGraph = new DirectedGraph();
+                visualizer = new PackageUpgradeVisualizer(directedGraph);
+            }
+
+            [Fact]
+            public void ThrowsOnNullPackage()
+            {
+                Assert.Throws<ArgumentNullException>("package", () => visualizer.Visualize(null, Enumerable.Empty<PackageUpgrade>()));
+            }
+
+            [Fact]
+            public void AcceptsNullUpgrades()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+
+                visualizer.Visualize(package, null);
+
+                Assert.Equal(1, directedGraph.Nodes.Length);
+                Assert.Null(directedGraph.Links);
+            }
+
+            [Fact]
+            public void CreatesNodeWithIdAndVersionOfPackage()
+            {
+                var package = CreatePackage();
+
+                visualizer.Visualize(package, Enumerable.Empty<PackageUpgrade>());
+
+                var node = directedGraph.Nodes[0];
+                Assert.Equal("A 1.2.4", node.Id);
+                Assert.Equal("A 1.2.4", node.Label);
+            }
+
+            [Fact]
+            public void UsesExistingNodes()
+            {
+                directedGraph.Nodes = new[]
+                {
+                    new DirectedGraphNode { Id = "A 1.2.4", },
+                    new DirectedGraphNode { Id = "B 1.1.0", },
+                    new DirectedGraphNode { Id = "C", },
+                };
+                var package = CreatePackage();
+                var packageUpgradeWithPackage = CreatePackageUpgradeWithPackage();
+                var packageUpgradeWithoutPackage = CreatePackageUpgradeWithoutPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgradeWithPackage, packageUpgradeWithoutPackage, });
+
+                Assert.Equal(3, directedGraph.Nodes.Length);
+                Assert.Equal(2, directedGraph.Links.Length);
+            }
+
+            [Fact]
+            public void ReleasePackageIsNotColoredNode()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+
+                visualizer.Visualize(package, Enumerable.Empty<PackageUpgrade>());
+
+                Assert.Null(directedGraph.Nodes[0].Background);
+            }
+
+            [Fact]
+            public void PrereleasePackageIsGainsboroNode()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0-a");
+
+                visualizer.Visualize(package, Enumerable.Empty<PackageUpgrade>());
+
+                Assert.Equal("Gainsboro", directedGraph.Nodes[0].Background);
+            }
+
+            [Fact]
+            public void CreatesNodesForUpgrades()
+            {
+                var package = CreatePackage();
+                var packageUpgradeWithPackage = CreatePackageUpgradeWithPackage();
+                var packageUpgradeWithoutPackage = CreatePackageUpgradeWithoutPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgradeWithPackage, packageUpgradeWithoutPackage, });
+
+                Assert.Equal(3, directedGraph.Nodes.Length);
+            }
+
+            [Fact]
+            public void CreatesNodeWithIdAndVersionOfPackageUsedForUpgradeIfExisting()
+            {
+                var package = CreatePackage();
+                var packageUpgrade = CreatePackageUpgradeWithPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                var node = directedGraph.Nodes.ElementAt(1);
+                Assert.Equal("B 1.1.0", node.Id);
+                Assert.Equal("B 1.1.0", node.Label);
+            }
+
+            [Fact]
+            public void CreatesNodeWithIdOfPackageDependencyIfPackageUsedForUpgradeIsMissing()
+            {
+                var package = CreatePackage();
+                var packageUpgrade = CreatePackageUpgradeWithoutPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                var node = directedGraph.Nodes[1];
+                Assert.Equal("C", node.Id);
+                Assert.Equal("C", node.Label);
+            }
+
+            [Fact]
+            public void ConnectsNodeOfPackageWithNodeOfDependency()
+            {
+                var package = CreatePackage();
+                var packageUpgradeWithPackage = CreatePackageUpgradeWithPackage();
+                var packageUpgradeWithoutPackage = CreatePackageUpgradeWithoutPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgradeWithPackage, packageUpgradeWithoutPackage, });
+
+                Assert.Equal("A 1.2.4", directedGraph.Links[0].Source);
+                Assert.Equal("B 1.1.0", directedGraph.Links[0].Target);
+
+                Assert.Equal("A 1.2.4", directedGraph.Links[1].Source);
+                Assert.Equal("C", directedGraph.Links[1].Target);
+            }
+
+            [Fact]
+            public void CreatesLinkWithVersionSpecOfPackageDependency()
+            {
+                var package = CreatePackage();
+                var packageUpgrade = CreatePackageUpgradeWithPackage();
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("[1.0.0]", directedGraph.Links[0].Label);
+            }
+
+            [Fact]
+            public void NoneUpgradeActionIsBlackLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.None,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("Black", directedGraph.Links[0].Stroke);
+            }
+
+            [Fact]
+            public void MinVersionUpgradeActionIsForestGreenLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.MinVersion,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("ForestGreen", directedGraph.Links[0].Stroke);
+            }
+
+            [Fact]
+            public void ReleaseToReleaseUpgradeActionIsGoldenrodLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.ReleaseToRelease,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("Goldenrod", directedGraph.Links[0].Stroke);
+            }
+
+            [Fact]
+            public void PrereleaseToReleaseUpgradeActionIsDarkOrangeLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.PrereleaseToRelease,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("DarkOrange", directedGraph.Links[0].Stroke);
+            }
+
+            [Fact]
+            public void PrereleaseToPrereleaseUpgradeActionIsOrangeRedLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.PrereleaseToPrerelease,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("OrangeRed", directedGraph.Links[0].Stroke);
+            }
+
+            [Fact]
+            public void ReleaseToPrereleaseUpgradeActionIsFirebrickLink()
+            {
+                var package = StubPackageFactory.CreatePackage("A", "1.0.0");
+                var packageUpgrade = new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.ReleaseToPrerelease,
+                    null);
+
+                visualizer.Visualize(package, new[] { packageUpgrade, });
+
+                Assert.Equal("Firebrick", directedGraph.Links[0].Stroke);
+            }
+
+            private static IPackage CreatePackage()
+            {
+                return StubPackageFactory.CreatePackage("A", "1.2.4");
+            }
+
+            private static PackageUpgrade CreatePackageUpgradeWithoutPackage()
+            {
+                return new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("C", "2.2.0"),
+                    PackageUpgradeAction.None,
+                    null);
+            }
+
+            private static PackageUpgrade CreatePackageUpgradeWithPackage()
+            {
+                return new PackageUpgrade(
+                    StubPackageDependencyFactory.CreateExact("B", "1.0.0"),
+                    PackageUpgradeAction.None,
+                    StubPackageFactory.CreatePackage("B", "1.1.0"));
+            }
+        }
+    }
+}
